55e1129e1f18946e32faf0f11f340480a96ba3e6
Jonas Follesø <jonas@follesoe.no>
2014-09-06 18:36:43 -0600

    Added howto and about pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,14 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/howto', {
+        templateUrl: 'views/howto.html',
+        controller: 'HowtoCtrl'
+      })
       .otherwise({ 
         redirectTo: '/'
       });
