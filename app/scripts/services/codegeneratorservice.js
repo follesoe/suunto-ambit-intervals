d2913bf000da2fa1997dcafefb09b0598555ca84
Jonas Follesø <jonas@follesoe.no>
2014-09-10 21:24:45 -0600

    Major work on code generation
    Do no longer flatten repeats, instead use logical OR and avoid
    re-generating the same code for laps within a repeat. This makes the
    apps much smaller.

--- app/scripts/services/codegeneratorservice.js
+++ app/scripts/services/codegeneratorservice.js
@@ -33,10 +33,10 @@ angular.module('ambitIntervalsApp')
 
       function loopSteps (steps) {
         for (var i = 0; i < steps.length; ++i) {
-          if (input.steps[i].type === 'Repeat') {
-            loopSteps(input.steps[i].steps);
+          if (steps[i].type === 'Repeat') {
+            loopSteps(steps[i].steps);
           } else {
-            convertStep(input.steps[i]);
+            convertStep(steps[i]);
           }
         }
       }
@@ -45,88 +45,143 @@ angular.module('ambitIntervalsApp')
       return input;
     };
 
-    var flattenRepeats = function (input) {
-      var output = JSON.parse(JSON.stringify(input));
-      output.steps = [];
+    var createHeader = function (input, appType) {
+      var output = '';
+      output += '/* ' + appType + ' App */\r\n';
+      output += '/* ' + input.name + ' */\r\n';
 
-      for (var i = 0; i < input.steps.length; ++i) {
-        if (input.steps[i].type === 'Repeat') {
-          var repeatSteps = input.steps[i].steps;
-          var repeatTimes = input.steps[i].times;
+      if (input.description) {
+        output += '/* ' + input.description + ' */\r\n';
+      }
 
-          for (var j = 0; j < repeatTimes; ++j) {
-            for (var k = 0; k < repeatSteps.length; ++k) {
-              var clonedStep = JSON.parse(JSON.stringify(repeatSteps[k]));
-              output.steps.push(clonedStep);
-            }
-          }
+      output += '\r\n';
+      return output;
+    };
 
-        } else {
-          output.steps.push(input.steps[i]);
-        }
+    var createVariableInitialization = function (variables) {
+      var output = '';
+      output += '/* Initialize variables */\r\n';
+      output += 'if (SUUNTO_DURATION == 0) {\r\n';
+      for (var i = 0; i < variables.length; ++i) {
+        output += '  ' + variables[i] + ' = 0;\r\n';
+      }
+      output += '}\r\n\r\n';
+      return output;
+    };
+
+    var createStepBodyForDuration = function (step) {
+      var output = '';
+
+      if (step.type === 'WarmUp') {
+        output += '  prefix = "wu";\r\n';
+      }
+
+      if (step.type === 'CoolDown') {
+        output += '  prefix = "cd";\r\n';
+      }
+
+      if (step.type === 'Interval') {
+        output += '  prefix = "int";\r\n';
+      }
+
+      if (step.type === 'Recovery') {
+        output += '  prefix = "rec";\r\n';
+      }
+
+      if (step.type === 'Rest') {
+        output += '  prefix = "rest";\r\n';
+      }
+
+      if (step.duration.type === 'Lap') {
+        output += '  postfix = "m";\r\n';
+        output += '  RESULT = SUUNTO_LAP_DISTANCE*1000;\r\n';
+      }
+
+      if (step.duration.type === 'Distance') {
+        output += '  postfix = "m";\r\n';
+        output += '  RESULT = ' + (step.duration.value*1000) + ' - (SUUNTO_LAP_DISTANCE*1000);\r\n';
       }
 
       return output;
     };
 
-    var createHeader = function (input) {
-      var output = '/* ' + input.name + ' */\r\n';
-      if (input.description) {
-        output += '/* ' + input.description + ' */\r\n\r\n';
-      } else {
-        output += '\r\n';
+    var createStepBodyForTarget = function (step) {
+      var output = '';
+
+      if (step.target.type === 'None') {
+        output += '  prefix = "at";\r\n';
+        output += '  postfix = "/km";\r\n';
+        output += '  TARGETPACE = ACTUALPACE;\r\n';
+        output += '  TARGETSEC = Suunto.mod(TARGETPACE, 60);\r\n';
+        output += '  TARGETMIN = (TARGETPACE - TARGETSEC) / 60;\r\n';
+        output += '  RESULT = TARGETMIN + TARGETSEC/100;\r\n';
+      }
+
+      if (step.target.type === 'Pace') {
+        output += '  prefix = "to";\r\n';
+        output += '  postfix = "/km";\r\n';
+        output += '  TARGETPACE = ' + step.target.to + ';\r\n';
+        output += '  TARGETSEC = Suunto.mod(TARGETPACE, 60);\r\n';
+        output += '  TARGETMIN = (TARGETPACE - TARGETSEC) / 60;\r\n';
+        output += '  RESULT = TARGETMIN + TARGETSEC/100;\r\n';
+      }
+
+      return output;
+    };
+
+    var createStepCommentForDuration = function (lapNumber, step) {
+      return '/* Lap ' + lapNumber +' is ' + step.type + ' with duration type ' + step.duration.type + ' */\r\n';
+    };
+
+    var createStepCommentForTarget = function (lapNumber, step) {
+      return '/* Lap ' + lapNumber +' is ' + step.type + ' with target type ' + step.target.type + ' */\r\n';
+    };
+
+    var createBody = function (input, commentFunction, stepFunction) {
+      var output = '';
+      var lapNumber = 0;
+      for (var i = 0; i < input.steps.length; ++i) {
+        lapNumber++;
+        var step = input.steps[i];
+
+        if (step.type === 'Repeat') {
+          for (var j = 0; j < step.steps.length; ++j) {
+            var ifCheck = [];
+            var repeatLapNumbers = [];
+            var repeatStep = step.steps[j];
+            for (var k = 0; k < step.times; ++k) {
+              var repeatLapNumber = lapNumber + j + (k * step.steps.length);
+              repeatLapNumbers.push(repeatLapNumber);
+              ifCheck.push('SUUNTO_LAP_NUMBER == ' + repeatLapNumber);
+            }
+
+            output += commentFunction(repeatLapNumbers.join(', '), repeatStep);
+            output += 'if (' + ifCheck.join(' || ') + ') {\r\n';
+            output += stepFunction(repeatStep);
+            output += '}\r\n\r\n';
+          }
+          lapNumber += (step.times * step.steps.length) - 1;
+        } else {
+          output += commentFunction(lapNumber, step);
+          output += 'if (SUUNTO_LAP_NUMBER == ' + lapNumber + ') {\r\n';
+          output += stepFunction(step);
+          output += '}\r\n\r\n';
+        }
       }
       return output;
     };
 
     this.generateDurationApp = function (interval) {
       var input = JSON.parse(JSON.stringify(interval));
-      input = flattenRepeats(input);
       input = convertPaceToSeconds(input);
 
       var output = '';
-      output += createHeader(input);
 
-      output += '/* Interval set - duration application */\r\n';
-      output += '/* Initialize variables */\r\n';
-      output += 'if (SUUNTO_DURATION == 0) {\r\n';
-      output += '  RESULT = 0;\r\n';
-      output += '}\r\n\r\n';
+      output += createHeader(input, 'Duration');
+      output += createVariableInitialization(['RESULT']);
+      output += createBody(input, createStepCommentForDuration, createStepBodyForDuration);
 
-      for (var i = 0; i < input.steps.length; ++i) {
-        var lapNumber = i + 1;
-        var step = input.steps[i];
-
-        output += '/* Lap ' + lapNumber +' is ' + step.type + ' with duration type ' + step.duration.type + ' */\r\n';
-        output += 'if (SUUNTO_LAP_NUMBER == ' + lapNumber + ') {\r\n';
-
-        if (step.type === 'WarmUp') {
-          output += '  prefix = "wu";\r\n';
-          output += '  postfix = "m";\r\n';
-          output += '  RESULT = SUUNTO_LAP_DISTANCE*1000;\r\n';
-        }
-
-        if (step.type === 'Interval' && step.duration.type === 'Distance') {
-          output += '  prefix = "int";\r\n';
-          output += '  postfix = "m";\r\n';
-          output += '  RESULT = ' + (step.duration.value*1000) + ' - (SUUNTO_LAP_DISTANCE*1000);\r\n';
-        }
-
-        if (step.type === 'Recovery' && step.duration.type === 'Distance') {
-          output += '  prefix = "rec";\r\n';
-          output += '  postfix = "m";\r\n';
-          output += '  RESULT = ' + (step.duration.value*1000) + ' - (SUUNTO_LAP_DISTANCE*1000);\r\n';
-        }
-
-        if (step.type === 'CoolDown') {
-          output += '  prefix = "cd";\r\n';
-          output += '  postfix = "m";\r\n';
-          output += '  RESULT = SUUNTO_LAP_DISTANCE*1000;\r\n';
-        }
-        output += '}\r\n\r\n';
-      }
-
-      output += '/* Check if duration is reached, if so altert to notify runner */\r\n';
+      output += '/* Check if duration is reached and use alarm to notify to hit lap button */\r\n';
       output += 'if (RESULT < 0) {\r\n';
       output += '  RESULT = 0;\r\n';
       output += '  Suunto.alarmBeep();\r\n';
@@ -138,52 +193,13 @@ angular.module('ambitIntervalsApp')
     this.generateTargetApp = function (interval) {
       var input = interval;
       input = JSON.parse(JSON.stringify(input));
-      input = flattenRepeats(input);
       input = convertPaceToSeconds(input);
 
       var output = '';
-      output += createHeader(input);
-
-      output += '/* Interval set - target application */\r\n';
-      output += '/* Initialize variables */\r\n';
-      output += 'if (SUUNTO_DURATION == 0) {\r\n';
-      output += '  ACTUALPACE = 0;\r\n';
-      output += '  TARGETPACE = 0;\r\n';
-      output += '  TARGETSEC = 0;\r\n';
-      output += '  TARGETMIN = 0;\r\n';
-      output += '  RESULT = 0;\r\n';
-      output += '}\r\n\r\n';
-
+      output += createHeader(input, 'Target');
+      output += createVariableInitialization(['ACTUALPACE', 'TARGETPACE', 'TARGETSEC', 'TARGETMIN', 'RESULT']);
       output += 'ACTUALPACE = SUUNTO_PACE*60;\r\n\r\n';
-
-      for (var i = 0; i < input.steps.length; ++i) {
-        var lapNumber = i + 1;
-        var step = input.steps[i];
-
-        output += '/* Lap ' + lapNumber +' is ' + step.type + ' with target type ' + step.target.type + ' */\r\n';
-        output += 'if (SUUNTO_LAP_NUMBER == ' + lapNumber + ') {\r\n';
-
-        if (step.target.type === 'None') {
-          output += '  prefix = "at";\r\n';
-          output += '  postfix = "/km";\r\n';
-          output += '  TARGETPACE = ACTUALPACE;\r\n';
-          output += '  TARGETSEC = Suunto.mod(TARGETPACE, 60);\r\n';
-          output += '  TARGETMIN = (TARGETPACE - TARGETSEC) / 60;\r\n';
-          output += '  RESULT = TARGETMIN + TARGETSEC/100;\r\n';
-        }
-
-        if (step.target.type === 'Pace') {
-          output += '  prefix = "to";\r\n';
-          output += '  postfix = "/km";\r\n';
-          output += '  TARGETPACE = ' + step.target.to + ';\r\n';
-          output += '  TARGETSEC = Suunto.mod(TARGETPACE, 60);\r\n';
-          output += '  TARGETMIN = (TARGETPACE - TARGETSEC) / 60;\r\n';
-          output += '  RESULT = TARGETMIN + TARGETSEC/100;\r\n';
-        }
-
-        output += '}\r\n\r\n';
-      }
-
+      output += createBody(input, createStepCommentForTarget, createStepBodyForTarget);
       return output;
     };
   });
