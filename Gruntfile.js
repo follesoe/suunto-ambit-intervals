4de84f16a31c927fb8d1145aa018327d0daa0443
Jonas Follesø <jonas@follesoe.no>
2014-09-13 13:22:17 -0600

    Add CNAME file to dist file list

--- Gruntfile.js
+++ Gruntfile.js
@@ -329,6 +329,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
+            'CNAME',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/*'
