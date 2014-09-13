f9e7ad1f217567ccfee885be2c508463e6370d28
Jonas Follesø <jonas@follesoe.no>
2014-09-12 18:06:12 -0600

    Adds support for selecting app code

--- app/scripts/directives/appoutput.js
+++ app/scripts/directives/appoutput.js
@@ -1,3 +1,4 @@
+/* global: window, document */
 'use strict';
 
 /**
@@ -8,12 +9,27 @@
  */
 angular.module('ambitIntervalsApp')
   .directive('appoutput', function () {
+    var selectNode = function (node) {
+      var selection = window.getSelection();
+      var range = document.createRange();
+      range.selectNode(node);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    };
+
     return {
       restrict: 'E',
       scope: '@',
       templateUrl: '/views/appoutput.html',
       link: function postLink($scope, element) {
 
+        $scope.selectDurationApp = function () {
+          selectNode(element.find('pre')[0]);
+        };
+
+        $scope.selectTargetApp = function () {
+          selectNode(element.find('pre')[1]);
+        };
       }
     };
   });
